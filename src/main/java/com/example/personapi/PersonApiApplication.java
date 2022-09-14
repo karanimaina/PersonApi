11d30c9a8bb5e3a031e721ab480520c839dde04e
felixmaina <karani.maina2010@gmail.com>
2022-09-14 13:11:11 +0300

    generate Person class

--- src/main/java/com/example/personapi/PersonApiApplication.java
+++ src/main/java/com/example/personapi/PersonApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/personapi/model/Person.java
@@ -0,0 +1,10 @@
+package com.example.personapi.model;
+
+import java.util.Date;
+
+public class Person {
+    private int id;
+    private String name;
+    private String location ;
+    private Date birthDate;
+}
