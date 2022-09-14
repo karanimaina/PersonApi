7eafe40bd15fc40b6e2554a04b60766aced2b55f
felixmaina <karani.maina2010@gmail.com>
2022-09-14 13:14:14 +0300

    add @Id column

--- src/main/java/com/example/personapi/model/Person.java
+++ src/main/java/com/example/personapi/model/Person.java
@@ -1,12 +1,18 @@
 package com.example.personapi.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 @Entity
-
+//@Table soecifies the table name
 
 public class Person {
+    @Id
+
     private int id;
+//    @Column specifies the columb mapping
     private String name;
     private String location ;
     private Date birthDate;
