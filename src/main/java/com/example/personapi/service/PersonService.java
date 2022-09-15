5075b2aa24cd9908ac7ca9f5273965e684fd4755
felixmaina <karani.maina2010@gmail.com>
2022-09-15 22:25:53 +0300

    get Allperson by id

--- src/main/java/com/example/personapi/service/PersonService.java
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -1,31 +1,31 @@
-//package com.example.personapi.service;//package com.example.personapi.service;
-//
-//import com.example.personapi.exception.ItemAlreadyExistsException;
-//import com.example.personapi.model.Gerson;
-//
-//import com.example.personapi.repository.GersonRepository;
-//import com.example.personapi.service.exception.ItemNotFoundException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//public class PersonService {
-//    @Autowired
-//    private GersonRepository  gersonRepository;
-//    public List<Gerson>getAllPersons(){
-//       return gersonRepository.findAllBySoftDeleteFalse();
-//    }
-//    public Gerson  getPersonById(long id){
-//        Gerson person =  gersonRepository.findByIdAndSoftDeleteFalse(id).orElse(null);
-//        if (person==null){
-//            throw  new ItemNotFoundException("person does not exist");
-//        }
-//        return person;
-//    }
+package com.example.personapi.service;//package com.example.personapi.service;
+
+import com.example.personapi.exception.ItemAlreadyExistsException;
+import com.example.personapi.model.Gerson;
+
+import com.example.personapi.repository.GersonRepository;
+import com.example.personapi.service.exception.ItemNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class PersonService {
+    @Autowired
+    private GersonRepository  gersonRepository;
+    public List<Gerson>getAllPersons(){
+       return gersonRepository.findAllBySoftDeleteFalse();
+    }
+    public Gerson  getPersonById(long id){
+        Gerson person =  gersonRepository.findByIdAndSoftDeleteFalse(id).orElse(null);
+        if (person==null){
+            throw  new ItemNotFoundException("person does not exist");
+        }
+        return person;
+    }
 //    public Gerson saveData(Gerson person){
 //        Gerson gerson = gersonRepository.findByNameAndSoftDeleteFalse(person.getName());
 //            gersonRepository.save(person);
